0fe93863af96ef9ec98990e35d8aab08904699c3
ismaelnrqz <ismaelnrqz@gmail.com>
2024-03-17 18:43:58 -0400

    new api

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,37 @@
-import HomePage from "./components/HomePage"
-import Search from "./components/SearchMusic"
+import HomePage from "./components/HomePage";
+import API_KEY from "../config";
+import { useEffect } from "react";
+import FetchData from "./utils/FetchData";
 
 function App() {
+  useEffect(() => {
+    const Times_API_URL = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`;
+
+    const doFetch = async () => {
+      const topStories = await FetchData(Times_API_URL);
+      console.log(topStories);
+    };
+    doFetch();
+
+    const Movie_Review_URL = `https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${API_KEY}`;
+    //https://api.nytimes.com/svc/news/v3/content/nyt/business.json?api-key=yourkey
+    //https://api.nytimes.com/svc/news/v3/content/nyt/world.json?api-key=yourkey
+    const Books_URL = `https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=${API_KEY}`;
+
+    const fetchData = async () => {
+      const movieReview = await FetchData(Movie_Review_URL);
+      const book = await FetchData(Books_URL);
+      console.log(movieReview);
+      console.log(book);
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
-    <HomePage />
-    <Search />
+      <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- app/src/components/HomePage.jsx
+++ app/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./NavBar"
 
 const HomePage = () => {
+
   return (
     <>
     < NavBar/>
