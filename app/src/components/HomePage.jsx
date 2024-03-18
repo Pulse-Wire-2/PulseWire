6dae15dcf2d2467609a9e211a92e82cb40326465
ismaelnrqz <ismaelnrqz@gmail.com>
2024-03-18 12:45:29 -0400

    rendered home

--- app/src/components/HomePage.jsx
+++ app/src/components/HomePage.jsx
@@ -1,10 +1,13 @@
 import NavBar from "./NavBar"
+import RenderTopStories from "./RenderTopStories"
 
 const HomePage = () => {
 
   return (
     <>
-    < NavBar/>
+    <NavBar/>
+    <h1>Top Stories Today</h1>
+    <RenderTopStories />
     </>
   )
 }
---
+++ app/src/components/RenderTopStories.jsx
@@ -0,0 +1,40 @@
+import API_KEY from "../../config";
+import FetchData from "../utils/FetchData";
+import { useState, useEffect } from "react";
+
+const RenderTopStories = () => {
+  const [results, setResults] = useState([]);
+  
+  useEffect(() => {
+    const Times_API_URL = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`;
+
+    const doFetch = async () => {
+      const response  = await FetchData(Times_API_URL);
+      const response_data = response[0];
+      const {results} = response_data;
+      console.log(results) 
+      setResults(results);
+    };
+    doFetch();
+  }, []);
+  
+  return (
+    <div className="Container">
+      {results.map((story) => (
+        <div key={story.url}>
+          <img src={story.multimedia?.[0]?.url} alt={story.title} />
+          <h3>{story.title}</h3>
+          <h3>{story.byline}</h3>
+          <p>{story.abstract}</p>
+          <button>
+            <a href={story.url} target="_blank" rel="noopener noreferrer">
+              Read More
+            </a>
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RenderTopStories
