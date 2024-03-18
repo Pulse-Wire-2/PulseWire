e8cbb0a4d42709be88d236e7c293833ae0d2f188
ismaelnrqz <ismaelnrqz@gmail.com>
2024-03-17 22:20:14 -0400

    changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -13,15 +13,28 @@ function App() {
     };
     doFetch();
 
-    const Movie_Review_URL = `https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=${API_KEY}`;
-    //https://api.nytimes.com/svc/news/v3/content/nyt/business.json?api-key=yourkey
+
+    /*
+    This will be the third page. It will need two inputs to fetch. 
+    EndPoint: https://api.nytimes.com/svc/news/v3//content/{source}/{section}.json?api-key=yourkey
+    1. Source {all, nyt, inyt}
+    2. Section {50 possible list (drop down menu)}
+
+    */
+    const newsWire = `https://api.nytimes.com/svc/news/v3/content/nyt/business.json?api-key=${API_KEY}`;
+    //https://api.nytimes.com/svc/news/v3/content/nyt/business.json?api-key=yourkey 
     //https://api.nytimes.com/svc/news/v3/content/nyt/world.json?api-key=yourkey
-    const Books_URL = `https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=${API_KEY}`;
+    
+    // const Books_URL = `const apiUrl = https://api.nytimes.com/svc/books/v3/reviews.json?author=${authorName}&api-key=y=${API_KEY}`;
+    const authorName = "Stephen King";
+    const Books_URL = `https://api.nytimes.com/svc/books/v3/reviews.json?author=${authorName}&api-key=${API_KEY}`;
+
+    // https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json
 
     const fetchData = async () => {
-      const movieReview = await FetchData(Movie_Review_URL);
+      const realTimeNews = await FetchData(newsWire);
       const book = await FetchData(Books_URL);
-      console.log(movieReview);
+      console.log(realTimeNews);
       console.log(book);
     };
     fetchData();
