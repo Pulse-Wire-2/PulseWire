a5ecf5dccca289aeb7844fdc66c0040da768ad61
ismaelnrqz <ismaelnrqz@gmail.com>
2024-03-20 17:02:49 -0400

    context

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 
 const NavBar = () => {
   return (
+    <>
     <nav>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/search">BestSeller</Link>
-        </li>
-        <li>
-          <Link to="/Latest-News"> Latest News</Link>
-        </li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/BestSeller">BestSeller</Link></li>
+        <li><Link to="/Latest-News"> Latest News</Link></li>
       </ul>
     </nav>
+    <button>Toggle Theme</button>
+    </>
   );
 };
 
